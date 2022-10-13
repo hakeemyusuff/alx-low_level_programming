e3c3dc761d38b5b4de858e881b381ee771b20282
Yusuff <hakeemyusuff19@gmail.com>
2022-10-13 14:33:41 -0700

    .

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - printing to standard error
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
