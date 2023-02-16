185b6ff5ea196dcbb475fa3d3f8e789e56896b6f
Yusuff <hakeemyusuff19@gmail.com>
2023-02-16 08:04:48 -0800

    fixed some errors and updated new task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * Main- entry point
+ * main - Entry point
  *
  * Return: Always 0 (sucess)
  */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
