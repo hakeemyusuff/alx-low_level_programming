9d9a15bcb5b1a53720e87d724f167e8e06403697
Abdulhakeem Yusuff <hakeemyusuff19@gmail.com>
2023-03-10 23:18:41 +0100

    fixed errors

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,8 +7,8 @@
  * Return: 0 on sucess
  */
 
-int main(int argc, char __attribute__((__unused__)) **argv)
+int main(int argc, char __attribute__((__unused__)) *argv[])
 {
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 	return (0);
 }
