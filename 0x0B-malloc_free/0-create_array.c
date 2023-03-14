50d4f9458a4e22dcb637c5f1da51e6a4332a84e1
Abdulhakeem Yusuff <hakeemyusuff19@gmail.com>
2023-03-14 22:12:16 +0100

    betty error fixed

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,14 +15,16 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 	{
-		return NULL;
+		return (NULL);
 	}
 	arr = malloc(size * sizeof(char));
-	if (arr == NULL) {
-		return NULL;
+	if (arr == NULL)
+	{
+		return (NULL);
 	}
-	for (i = 0; i < size; i++) {
+	for (i = 0; i < size; i++)
+	{
 		arr[i] = c;
 	}
-	return arr;
+	return (arr);
 }
