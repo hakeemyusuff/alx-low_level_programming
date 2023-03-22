8b5e2f61e0647ffa7380e5adfd8bcd1d032b18fa
Abdulhakeem Yusuff <hakeemyusuff19@gmail.com>
2023-03-22 23:19:52 +0100

    created a new directory and task 1 of mandatory task added README.md also created

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,14 @@
+#include "function_pointer.h"
+
+/**
+ * array_iterator - executes a function given as a parameter
+ * @array - array of numbers
+ * @size - the size of the array
+ * @action - function pointers
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	
+}
